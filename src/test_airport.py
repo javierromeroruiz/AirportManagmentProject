d66addf2fb8fbfb52fdb6b052226d2009dfe7233
Jahuerss <jahueromero@gmail.com>
2026-05-18 20:45:00 +0200

    Update

---
+++ src/test_airport.py
@@ -0,0 +1,20 @@
+from airport import *
+airportadd = Airport ("EGLL", 41.297445, 2.0832941)
+airportremove = "BIKF"
+"""SetSchengen(airport)
+PrintAirport (airport)"""
+
+filename = "Airports.txt"
+filename2 = "SchengenAirports.txt"
+airports_list = LoadAirports (filename)
+
+i = 0
+while i < len(airports_list):
+    SetSchengen(airports_list[i])
+    i += 1
+
+SaveSchengenAirports (airports_list, filename2)
+AddAirport(airports_list, airportadd)
+RemoveAirport(airports_list, airportremove)
+
+MapAirports(airports_list, "Mapa.kml")
